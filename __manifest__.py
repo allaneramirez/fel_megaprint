3b3772eb39afa88af194bef8e5fa9d8b5906322c
garzo94 <alexgarzo25@gmail.com>
2025-02-17 21:04:47 -0600

    first commit

--- __manifest__.py
+++ __manifest__.py
@@ -1,21 +1,19 @@
-# -*- encoding: utf-8 -*-
-
 {
     'name': 'FEL Mega Print',
-    'version': '0.34',
+    'version': '17.0.1',
     'category': 'Account',
     'description': """ Integra Odoo y factura electrónica con el CERTIFICADOR Mega Print, Genera DTO locales y DTO locales en contingencia """,
     'author': 'Eduardo Cortez Paz, Allan Ramirez',
     'website': 'https://www.exeticsoluciona.com',
-    'depends': ['account','l10n_gt_extra'],
+    'depends': ['account','l10n_gt_extra','account_accountant'],
     'data': [
-        'views/account_view.xml',
-        'views/partner_view.xml',
-        'views/contigencia.xml',
-        'report/reports.xml',
-        'report/report_invoice_rsm.xml',
+        # 'views/account_view.xml',
+        # 'views/partner_view.xml',
+        # 'views/contigencia.xml',
+        # 'report/reports.xml',
+        # 'report/report_invoice_rsm.xml',
     ],
+    'license': 'OPL-1',
     'demo': [],
     'installable': True
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
